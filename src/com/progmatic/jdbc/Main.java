9177f404121ffcc62d273090cc8339219226a56d
vasandris01 <undris01@gmail.com>
2023-01-23 14:10:27 +0100

    clean menu

--- src/com/progmatic/jdbc/Main.java
+++ src/com/progmatic/jdbc/Main.java
@@ -27,16 +27,19 @@ public class Main {
         System.out.println("Új rendelés felvétele(4)");
         System.out.println("Vissza a főmenübe(bármi más)");
     }
-    public void save_anything(){
+    public void save_anything(Controller controller){
         printSaveMenu();
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        sc.close();
         switch (s){
-            case "1" ->{}
+            case "1" ->{
+
+            }
             case "2" ->{}
             case "3" ->{}
-            case "4" ->{}
+            case "4" ->{
+                rendeles(controller, sc);
+            }
         }
     }
     public void start() {
@@ -53,48 +56,7 @@ public class Main {
             while (!(s = sc.nextLine()).equalsIgnoreCase("k")) {
                 switch (s.toLowerCase()) {
                     case "u" -> {
-                        System.out.println("Ki vagy koma? (id)");
-                        long vazon = Long.parseLong(sc.nextLine());
-                        List<Client> allC = controll.getAllClient();
-                        Client client = null;
-                        for (Client c : allC) {
-                            if (c.cid() == vazon) {
-                                client = c;
-                                break;
-                            }
-                        }
-                        if (client == null) {
-                            //új vásárló
-                        }
 
-                        List<Order> orders = controll.getAllOrder();
-                        long razon = orders.get(0).oid();
-                        for (int i = 1; i < orders.size(); i++) {
-                            if (razon < orders.get(i).oid()) {
-                                razon = orders.get(i).oid();
-                            }
-                        }
-                        razon++;
-
-                        List<Pizza> allP = controll.getAllPizza();
-                        List<OrderItem> orderItems = new LinkedList<>();
-                        for (Pizza p : allP) {
-                            System.out.println("Hány db " + p.name() + " kérsz?");
-                            short db = Short.parseShort(sc.nextLine());
-                            if (db > 0) {
-                                orderItems.add(new OrderItem(razon, p, db));
-                            }
-                        }
-                        List<Courier> couriers = controll.getAllCourier();
-                        Courier courier = couriers.get((int) (Math.random() * couriers.size()));
-
-
-                        Order order = new Order(razon, client, courier, orderItems, LocalDateTime.now());
-
-                        controll.addOrdder(order);
-                        for (OrderItem o : orderItems) {
-                            controll.addOrderItem(o);
-                        }
                     }
 
                     case "s" -> System.out.println("kereses");
@@ -149,5 +111,50 @@ public class Main {
     public static void main(String[] args) {
         Main m = new Main();
         m.start();
+    }
+
+    public void rendeles(Controller controll, Scanner sc){
+        System.out.println("Ki vagy koma? (id)");
+        long vazon = Long.parseLong(sc.nextLine());
+        List<Client> allC = controll.getAllClient();
+        Client client = null;
+        for (Client c : allC) {
+            if (c.cid() == vazon) {
+                client = c;
+                break;
+            }
+        }
+        if (client == null) {
+            //új vásárló
+        }
+
+        List<Order> orders = controll.getAllOrder();
+        long razon = orders.get(0).oid();
+        for (int i = 1; i < orders.size(); i++) {
+            if (razon < orders.get(i).oid()) {
+                razon = orders.get(i).oid();
+            }
+        }
+        razon++;
+
+        List<Pizza> allP = controll.getAllPizza();
+        List<OrderItem> orderItems = new LinkedList<>();
+        for (Pizza p : allP) {
+            System.out.println("Hány db " + p.name() + " kérsz?");
+            short db = Short.parseShort(sc.nextLine());
+            if (db > 0) {
+                orderItems.add(new OrderItem(razon, p, db));
+            }
+        }
+        List<Courier> couriers = controll.getAllCourier();
+        Courier courier = couriers.get((int) (Math.random() * couriers.size()));
+
+
+        Order order = new Order(razon, client, courier, orderItems, LocalDateTime.now());
+
+        controll.addOrdder(order);
+        for (OrderItem o : orderItems) {
+            controll.addOrderItem(o);
+        }
     }
 }
